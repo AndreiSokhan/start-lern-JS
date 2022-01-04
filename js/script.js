6167328dee77fd9ef73ba65741d496be343be52e
andreisokhan <sokhanpageproofs@gmail.com>
2022-01-04 23:03:22 +0300

    008 end

---
+++ js/script.js
@@ -0,0 +1,64 @@
+"use strict";
+
+
+
+//004-006
+
+
+//let number = 5.6;
+
+//console.log(-5/0);
+//console.log('string' * 9);
+
+//const persone = `5`;
+
+//const bool = false;
+
+//console.log(something);
+
+//let und;
+//console.log(und);
+
+//const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false
+//};
+
+//console.log(obj.isMarried);
+//console.log(obj["name"]);
+
+//let arr = ['plum.png', 'orange.jpeg', 6, 'apple.bmp', {}]
+//console.log(arr[4]);
+
+//007
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log((answer + 5));
+
+//const answers = [];
+
+//answers[0] = prompt('Как Ваше имя?', '');
+//answers[1] = prompt('Как Ваша фамилия ?', '');
+//answers[2] = prompt('Сколько Вам лет?', '');
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
+
+
+//008
+
+//const category = 'toys';
+
+//console.log('https://someurl.com/' + category + '/' + '4');
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = 'Andrey';
+
+//alert(`Привет, ${user}`);
