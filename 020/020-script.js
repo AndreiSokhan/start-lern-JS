192690d49491112d35fd360324724386a257cdfc
Andrei <sokhanpageproofs@gmail.com>
2022-01-19 23:48:37 +0300

    020 added

--- 020/020-script.js
+++ 020/020-script.js
@@ -1,5 +1,11 @@
 // методы объектов
 
+
+//объекты - это структуры, которые могут сохранять в себе любые типы даных в формате "ключ: значение". Объекты могут быть вложены друг в друга
+//во внутрь объекта можно записывать функции и таким образом создаются методы нащешего объекта
+
+//свойства акцессоры "get" и "set" 
+
 "use strict";
 
 //создаем объект дла работы
@@ -11,8 +17,21 @@ const option = {
    colors: {
       border: 'black',
       bg: 'red'
+   },
+   makeTest: function() {
+      console.log("Test");
    }
 };
+
+option.makeTest();
+
+
+//деструктуризация объекта (вытаскиваем объект из вложенного в качестве отдельной переменной)
+const {border, bg} = option.colors;
+console.log(border);
+
+//ключи нашего объекта (получаем массив со всеми ключами нашего объекта) и мы можем использовать св-во length, для того чтобы узнать длинну массива   
+//console.log(Object.keys(option).length);
 
 //выводим в панель из объекта option по ключу name значение 'test'
 //console.log(option.name);
@@ -20,10 +39,40 @@ const option = {
 //удаляем пару из объекта opition 
 
 //delete option.name;
-
 //console.log(option);
 
 //перебор
-for (let key in option) {
-   console.log(`Свойство ${key} имеет значение ${option[key]}`);
-}
+//for (let key in option) {
+//   if (typeof(option[key]) === 'object') {
+//      for (let i in option[key]) {
+//         console.log(`Свойство ${i} имеет значение ${option[key][i]}`);
+//      }
+//   } else {
+//      console.log(`Свойство ${key} имеет значение ${option[key]}`);
+//   }
+//}
+
+//создаем счетчик для перебора 
+
+//let counter = 0;
+//for (let key in option) {
+//   if (typeof(option[key]) === 'object') {
+//      for (let i in option[key]) {
+//         console.log(`Свойство ${i} имеет значение ${option[key][i]}`);
+//         counter ++;
+//      }
+//   } else {
+//      console.log(`Свойство ${key} имеет значение ${option[key]}`);
+//      counter ++;
+//   }
+//}
+
+//console.log(counter);
+
+//узнаем какое количество свойств находится в объекте
+//let counter = 0;
+//for (let key in option) {
+//   counter++;
+//}
+
+//console.log(counter);
