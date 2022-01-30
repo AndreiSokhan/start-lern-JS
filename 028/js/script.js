a0bef2c99c12f7473bf26d44b0e12566619006b8
Andrei <sokhanpageproofs@gmail.com>
2022-01-30 21:56:05 +0300

    objects retrieved from pages

--- 028/js/script.js
+++ 028/js/script.js
@@ -1,4 +1,4 @@
-//получение объектов со страницы
+// 
 
 'use strict';
 
@@ -45,6 +45,42 @@ const box = document.getElementById('box'),
       btns = document.getElementsByTagName('button'),
       circles = document.getElementsByClassName('circle'),
       hearts = document.querySelectorAll('.heart'),
-      oneHeart = document.querySelector('.heart');
+      oneHeart = document.querySelector('.heart'),
+      wrapper = document.querySelector('wrapper');
 
-console.dir(box);
+//будем изменять стили элементов
+
+box.style.backgroundColor = 'green';
+box.style.width = '300px';
+
+box.style.cssText = 'background-color: aqua; width: 500px';
+
+btns[1].style.borderRadius = '50%',
+btns[1].style.backgroundColor = 'red';
+
+circles[1].style.backgroundColor = 'red';
+
+//циклы для изменения элементов практически никогда не используются
+//for (let i = 0; i < hearts.length; i++) {
+//   hearts[i].style.backgroundColor = 'orange';
+//};
+
+//почти всегда используются перебирающие методы
+hearts.forEach(item => {
+   item.style.backgroundColor = 'orange';
+});
+
+// изучение основных методов для работы с элементами страницы
+
+const div = document.createElement('div');
+//текстовые узлы - элементы без оболочки тега
+
+//const text = document.createTextNode('Тут был я');
+
+//стилизация элемента, которого нет в html разметке, а который был создан в js
+div.classList.add('black');
+
+//document.body.append(div);
+//wrapper.append(div);
+
+wrapper.prepend(div);
