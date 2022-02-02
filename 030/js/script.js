306819904c3e4660f222998e9d04d182ae67641b
Andrei <sokhanpageproofs@gmail.com>
2022-02-02 23:40:48 +0300

    exercise completed

--- 030/js/script.js
+++ 030/js/script.js
@@ -24,3 +24,45 @@ const movieDB = {
     ]
 };
 
+
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    moviList = document.querySelector('.promo__interactive-list');
+
+
+
+adv.forEach(item => {
+    item.remove();
+});
+
+//еще один вариант с обычной (не стрелочной функцикцией)
+//adv.forEach(function(item) {
+//    item.remove();
+//});
+
+//меняем жанр комедия на драма
+genre.textContent = 'драма';
+
+//меняем backgroundImg
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+//очищаем список фильмов который записан в HTML верстке и "захардкожен"
+moviList.innerHTML = "";
+
+//сортируем фильмы из базы данных JS по алфавиту
+movieDB.movies.sort();
+
+//закидывае фильмьмы из базы данных JS на страницу
+movieDB.movies.forEach((film, i) => {
+    moviList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
+
+
+// '+=' - это опреатор дополнитеоного присваивания выражения приведенные в примере ниже будут аналогичны (отличаются только дишь формой записи). Работает как с числами так и со строками
+// a = a + 1;
+// a += 1;
