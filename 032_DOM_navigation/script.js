662c76cc0a45901c9504d21efc5647e724259eb5
Andrei <sokhanpageproofs@gmail.com>
2022-02-08 23:36:44 +0300

    exercise

--- 032_DOM_navigation/script.js
+++ 032_DOM_navigation/script.js
@@ -1,12 +1,25 @@
+//обращение к head через js
 //console.log(document.head);
+
+//получение всех элементов html
 //console.log(document.documentElement);
+
+//получение всех элементов, которые явл. детьми для body
 //console.log(document.body.childNodes);
+
+//получение первого ребенка для body
 //console.log(document.body.firstChild);
+
+//
 //console.log(document.body.firstElementChild);
+
+//получение последнего ребенка для body
 //console.log(document.body.lastChild);
 
-
+//получение родителя по id
 //console.log(document.querySelector('#current').parentNode);
+
+
 //console.log(document.querySelector('#current').parentElement);
 
 //console.log(document.querySelector('[data-current="3"]').nextElementSibling);
